1de8ba1fef257d61ec2449e5264e486e4c354618
Sergey <sergstas0@gmail.com>
2021-02-17 10:15:49 +0500

    [GYM]: init & basic package management

---
+++ GymnasticsHelper/app/src/main/java/com/sergstas/gymnasticshelper/feature/menu/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sergstas.gymnasticshelper.feature.menu.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sergstas.gymnasticshelper.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
